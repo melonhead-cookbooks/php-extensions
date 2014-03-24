804adc14b7912b16606ef8863cea1c04917f6330
Danny Knapp <dknapp@salesforce.com>
2014-03-24 01:19:15 -0700

    Added an installer for PEAR

---
+++ recipes/pear.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: php-extensions
+# Recipe:: default
+#
+# Copyright (C) 2014 Salesforce Pardot
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+execute "download latest pear" do
+  command "wget http://pear.php.net/install-pear-nozlib.phar"
+  action :run 
+end
+
+execute "install latest pear" do
+  command "php install-pear-nozlib.phar"
+  action :run
+end
+
+# update the main channels
+php_pear_channel 'pear.php.net' do
+  action :update
+end
+
+php_pear_channel 'pecl.php.net' do
+  action :update
+end
+
