c377db9161d50f2ffa430c32b84a3ab835469885
Danny Knapp <dknapp@salesforce.com>
2014-06-20 23:33:13 -0700

    Fixing memcached

--- recipes/module_memcache.rb
+++ recipes/module_memcache.rb
@@ -21,14 +21,10 @@
 
 case node['platform_family']
 when 'rhel', 'fedora'
-  %w{ zlib-devel }.each do |pkg|
+  %w{ php-pecl-memcached }.each do |pkg|
     package pkg do
       action :install
     end
-  end
-  php_pear 'memcache' do
-    action :install
-    # directives(:shm_size => "128M", :enable_cli => 0)
   end
 when 'debian'
   package 'php5-memcache' do
