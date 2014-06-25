ec32606d8fa434fd675bd5b638c0c477297d16fb
Danny Knapp <dknapp@salesforce.com>
2014-06-25 16:46:41 -0700

    Fixing version headaches with FPDF installation

--- recipes/module_fpdf.rb
+++ recipes/module_fpdf.rb
@@ -5,26 +5,6 @@
 #
 # Copyright 2014, Salesforce
 #
-
-case node['platform_family']
-when 'rhel', 'fedora'
-  pearhub_chan = php_pear_channel 'pearhub.org' do
-    action :discover
-  end
-  php_pear 'FPDF' do
-    channel pearhub_chan.channel_name
-    action :install
-  end
-  file '/etc/php.d/fpdf.ini' do
-    action :create
-    content "extension=fpdf.so\n"
-    owner 'root'
-    group 'root'
-    mode 0644
-    notifies :restart, resources(service: 'apache2')
-  end
-when 'debian'
-  package 'php-fpdf' do
-    action :install
-  end
+package 'php-fpdf' do
+  action :install
 end
