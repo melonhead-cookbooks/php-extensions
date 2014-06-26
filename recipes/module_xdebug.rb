201bf978b0a29547cc9b8515284896a3be988131
Danny Knapp <dknapp@salesforce.com>
2014-06-26 13:04:29 -0700

    Abstracted xdebug parameters

--- recipes/module_xdebug.rb
+++ recipes/module_xdebug.rb
@@ -11,10 +11,21 @@ when 'rhel', 'fedora'
   php_pear 'xdebug' do
     action :install
     zend_extensions ['xdebug.so']
-    directives(remote_enable: 1)
+    directives(remote_enable: 1, idekey: 'vagrant')
   end
 when 'debian'
   package 'php5-xdebug' do
     action :install
+  end
+end
+
+ruby_block 'set xdebug PAM configuration' do
+  block do
+    rc = Chef::Util::FileEdit.new('/etc/security/pam_env.conf')
+    rc.insert_line_if_no_match(
+      'PHP_IDE_CONFIG',
+      'PHP_IDE_CONFIG DEFAULT="serverName=vagrant_cli"'
+    )
+    rc.write_file
   end
 end
--- recipes/phpunit.rb
+++ recipes/phpunit.rb
@@ -33,25 +33,3 @@ execute 'install phpunit' do
   action :run
   only_if "test 0 -eq `phpunit --version | grep 'PHPUnit' | wc -l`"
 end
-
-ruby_block 'set xdebug php_ide_config' do
-  block do
-    rc = Chef::Util::FileEdit.new('/etc/security/pam_env.conf')
-    rc.insert_line_if_no_match(
-      'PHP_IDE_CONFIG',
-      'PHP_IDE_CONFIG DEFAULT="serverName=vagrant_cli"'
-    )
-    rc.write_file
-  end
-end
-
-# The xdebug cookbook is broken and doesn't let us change the idekey
-# We could fork the cookbook, but for now write the config item to its own file
-file '/etc/php.d/xdebug-idekey.ini' do
-  action :create
-  content "xdebug.idekey=vagrant\n"
-  owner 'root'
-  group 'root'
-  mode 0644
-  notifies :restart, resources(service: 'apache2')
-end
